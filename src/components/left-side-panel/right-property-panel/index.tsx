86726925ec5c5435112bacdbe56416db044a8fa6
feiandxs <yiziyint@gmail.com>
2024-04-17 08:04:37 +0000

    Add RightPropertyPanel component and MainComponent

---
+++ src/components/left-side-panel/right-property-panel/index.tsx
@@ -0,0 +1,7 @@
+const RightPropertyPanel = () => (
+  <div>
+    right
+  </div>
+);
+
+export { RightPropertyPanel };
---
+++ src/components/main/index.tsx
@@ -0,0 +1,7 @@
+const MainComponent = () => (
+  <div>
+    <h1>Main Component</h1>
+  </div>
+);
+
+export { MainComponent };
